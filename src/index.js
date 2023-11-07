93fb512de0fac41f4308e2983eb86239c174290e
Caleb Raney <110679107+calebraney@users.noreply.github.com>
2023-11-07 12:23:16 -0500

    basic horizontal setup

--- src/index.js
+++ src/index.js
@@ -4,6 +4,42 @@ document.addEventListener('DOMContentLoaded', function () {
 
   //////////////////////////////
   //Global Variables
+  const horizontalSection = '[horizontal-el="section"]';
+  const horizontalTrack = '[horizontal-el="track"]';
+  const horizontalStickyEl = '[horizontal-el="sticky"]';
+
+  const setTrackHeights = function () {
+    const sectionHeights = document.querySelectorAll(horizontalSection);
+    sectionHeights.forEach(function (section) {
+      let track = section.querySelector(horizontalTrack);
+      let trackWidth = track.offsetWidth;
+      section.style.height = trackWidth + 'px';
+    });
+  };
+
+  const horizontalScroll = function () {
+    const section = document.querySelector(horizontalSection);
+    const track = document.querySelector(horizontalTrack);
+    const stickyEl = document.querySelector(horizontalStickyEl);
+    if (!section || !track || !stickyEl) return;
+    //main timeline
+    let tlMain = gsap
+      .timeline({
+        scrollTrigger: {
+          trigger: section,
+          start: 'top top',
+          end: '98% bottom',
+          scrub: 1,
+        },
+      })
+      .set(stickyEl, {
+        overflow: hidden,
+      })
+      .to(track, {
+        xPercent: -100,
+        ease: 'none',
+      });
+  };
 
   //////////////////////////////
   //Control Functions on page load
@@ -20,8 +56,88 @@ document.addEventListener('DOMContentLoaded', function () {
       (context) => {
         let { isMobile, isTablet, isDesktop, reduceMotion } = context.conditions;
         // run animation functions
+        setTrackHeights();
       }
     );
   };
   gsapInit();
+  window.addEventListener('resize', function () {
+    setTrackHeights();
+  });
 });
+
+// let tlMain = gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".section-height",
+//       start: "top top",
+//       end: "98% bottom",
+//       scrub: 1
+//     }
+//   })
+//   .to(".track", {
+//     xPercent: -100,
+//     ease: "none"
+//   });
+
+// // hero photo
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".hero-panel",
+//       containerAnimation: tlMain,
+//       start: "left left",
+//       end: "right left",
+//       scrub: true
+//     }
+//   })
+//   .from(".hero-panel_img", { scale: 1.6 }, 0);
+
+// // note
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".note-panel",
+//       containerAnimation: tlMain,
+//       start: "left right",
+//       end: "left left",
+//       scrub: true
+//     }
+//   })
+//   .from(".note-panel_img", { rotate: 45, scale: 0.3 });
+
+// // thanks
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".thanks-panel_wrap",
+//       containerAnimation: tlMain,
+//       start: "left left",
+//       end: "right right",
+//       scrub: true
+//     }
+//   })
+//   .to(".thanks-panel", { xPercent: 100, ease: "none" })
+//   .to(".thanks-panel_photo", { scale: 1 }, 0)
+//   .fromTo(
+//     ".thanks-panel_contain.is-2",
+//     {
+//       clipPath: "polygon(100% 0%, 100% 0%, 100% 100%, 100% 100%)"
+//     },
+//     { clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", ease: "none" },
+//     0
+//   );
+
+// // stagger photos
+// gsap
+//   .timeline({
+//     scrollTrigger: {
+//       trigger: ".stagger-panel",
+//       containerAnimation: tlMain,
+//       start: "left right",
+//       end: "right left",
+//       scrub: true
+//     }
+//   })
+//   .from(".stagger-panel_img", { x: "100vw", stagger: { each: 0.05 } })
+//   .to(".stagger-panel_img", { scale: 0.5, stagger: { each: 0.05 } });
